c10d40842a9cc09a588d0b8ca2fac774ce6243e8
MaRNG <kubicka.marek@gmail.com>
2026-05-02 22:28:03 +0200

    feat: implement MusicBrainz artist fetch

---
+++ App/Infrastructure/ExternalClient/Dto/MusicBrainzArtistDto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Infrastructure\ExternalClient\Dto;
+
+final readonly class MusicBrainzArtistDto
+{
+    public function __construct(
+        public string $id,
+        public string $name,
+        public string $country,
+    )
+    {
+    }
+}
--- App/Infrastructure/ExternalClient/Module/MusicBrainzArtistHandler.php
+++ App/Infrastructure/ExternalClient/Module/MusicBrainzArtistHandler.php
@@ -2,7 +2,41 @@
 
 namespace App\Infrastructure\ExternalClient\Module;
 
+use App\Infrastructure\ExternalClient\Dto\MusicBrainzArtistDto;
+use App\Infrastructure\ExternalClient\Dto\MusicBrainzTrackDto;
+use GuzzleHttp\RequestOptions;
+use Nette\Utils\Json;
+
 final class MusicBrainzArtistHandler extends BaseMusicBrainzHandler
 {
+    public function findArtist(string $artistName): ?MusicBrainzArtistDto
+    {
+        $response = $this->client->get('/ws/2/artist/', [
+            RequestOptions::QUERY => [
+                'query' => sprintf('artist:%s', $artistName),
+                'fmt' => 'json',
+            ]
+        ]);
 
+        $jsonResponse = Json::decode($response->getBody()->getContents(), forceArrays: true);
+
+        if ($jsonResponse['artists'] === [])
+        {
+            return null;
+        }
+
+        foreach ($jsonResponse['artists'] as $artist)
+        {
+            if ($artist['name'] === $artistName)
+            {
+                return new MusicBrainzArtistDto(
+                    $artist['id'],
+                    $artist['name'],
+                    $artist['country']
+                );
+            }
+        }
+
+        return null;
+    }
 }
